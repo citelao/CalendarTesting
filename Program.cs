f3a6c73ed8f36356ad5405e2ef414f9d858fcab9
Ben Stolovitz <ben@stolovitz.com>
2025-06-24 11:58:22 -0400

    testing

--- Program.cs
+++ Program.cs
@@ -15,6 +15,15 @@ var hebrewFormatter = new DateTimeFormatter("longdate", ["he-IL"]);
 var formattedHebrewDate = hebrewFormatter.Format(calendar.GetDateTime());
 Console.WriteLine(formattedHebrewDate);
 
-var hebrew2Formatter = new DateTimeFormatter("longdate", ["he-IL"], "USA", CalendarIdentifiers.Hebrew, ClockIdentifiers.TwelveHour);
+var region = new GeographicRegion("ISR");
+Console.WriteLine($"Region Name: {region.DisplayName}");
+Console.WriteLine($"Region Code: {region.Code}");
+Console.WriteLine($"Region Code 2 letter: {region.CodeTwoLetter}");
+Console.WriteLine($"Region Code Three Letter: {region.CodeThreeLetter}");
+Console.WriteLine($"Region Code Three Digit: {region.CodeThreeDigit}");
+
+// Any of those properties work for the region.
+var regionId = "001"; // or .Code, .CodeTwoLetter, .CodeThreeLetter, .CodeThreeDigit
+var hebrew2Formatter = new DateTimeFormatter("longdate", ["he-IL"], regionId, CalendarIdentifiers.Hebrew, ClockIdentifiers.TwelveHour);
 var formattedHebrew2Date = hebrew2Formatter.Format(calendar.GetDateTime());
 Console.WriteLine(formattedHebrew2Date);
